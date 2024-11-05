5c11459fd86a80631cd745134dc6e6785016d2ec
unknown <septianahmad07@gmail.com>
2024-11-05 14:14:23 +0700

    Second Commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,7 @@ import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
+import boy2 from "../../img/boy2.png"
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
@@ -48,7 +49,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        {/* <img src={boy} alt="" /> */}
+        <img src={boy2} alt="" />
         {/* animation */}
         <motion.img
           initial={{ left: "-36%" }}
